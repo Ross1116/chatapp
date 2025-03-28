a705574037338662317cdcb12cca119bf37907b3
Ross1116 <roshan123243@gmail.com>
2025-03-28 21:23:36 +1100

    port 300 server

--- main.go
+++ main.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client:", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buff := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buff)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buff[:n]
+		fmt.Println(string(msg))
+
+		ws.Write([]byte("Thank you for the msg!"))
+	}
+}
+
+func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":3000", nil)
+}
